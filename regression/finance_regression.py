e940c7188066a1054384f189910b5b1f416a9d99
boundring <boundring@gmail.com>
2021-02-18 13:04:46 -0800

    Filename changed
    The filename extension has changed from .tgz to .tar.gz.

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """
     Starter code for the regression mini-project.
     
@@ -15,21 +13,24 @@
 
 import sys
 import pickle
+import numpy as np
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+#features_list = ["bonus", "long_term_incentive"]
+
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,13 +38,14 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression().fit(feature_train, target_train)
+print("Regression coefficient (slope):", reg.coef_)
+print("Regression intercept:", reg.intercept_)
 
-
-
-
-
-
+print('\nTraining data r-squared: ', reg.score(feature_train,target_train))
+print('Test data r-squared: ', reg.score(feature_test,target_test))
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -56,14 +58,12 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
-
-
-### draw the regression line, once it's coded
-try:
-    plt.plot( feature_test, reg.predict(feature_test) )
-except NameError:
-    pass
+# drawing trained regression
+plt.plot(feature_test, reg.predict(feature_test), color='b')
+# re-training regression against test data (with outlier)
+reg.fit(feature_test, target_test)
+print("Test-data regression coefficient (slope):", reg.coef_)
+plt.plot(feature_train, reg.predict(feature_train), color='r')
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
